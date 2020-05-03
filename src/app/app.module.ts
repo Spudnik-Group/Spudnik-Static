c62cc70544e1aced0048b510803071fa9138d5fe
Nate Richardson <hello@naterichardson.com>
2020-05-03 15:24:09 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { NavComponent } from './main/nav/nav.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+		NavComponent,
+    FooterComponent,
+    HomeComponent,
+    FeaturesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
+		FlexLayoutModule,
+		MatButtonModule,
+		MatMenuModule,
+		MatIconModule,
+		MatToolbarModule,
+		MatCardModule,
+		MatDividerModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
